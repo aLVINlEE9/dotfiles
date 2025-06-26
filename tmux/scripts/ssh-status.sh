2de3273efb9829f1156c08ea7a6e2b4c3e93a202
aLVINlEE9 <fldh3746@gmail.com>
2025-06-27 08:55:55 +0900

    fix: string format

--- tmux/scripts/ssh-status.sh
+++ tmux/scripts/ssh-status.sh
@@ -2,9 +2,9 @@
 
 if [ -n "$SSH_CONNECTION" ]; then
     SERVER_IP=$(echo "$SSH_CONNECTION" | awk '{print $3}')
-    printf "%-15s" "$USER@$SERVER_IP"
+    printf "%-18s" "$USER@$SERVER_IP"
 elif [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-    printf "%-15s" "$USER@ssh"
+    printf "%-18s" "$USER@ssh"
 else
-    printf "%-15s" "$USER@local"
+    printf "%-18s" "$USER@local"
 fi
