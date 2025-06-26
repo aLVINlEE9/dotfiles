eb85cdaec5e33ca9ed7ddc5208ef66374a2a4ed9
aLVINlEE9 <fldh3746@gmail.com>
2025-06-27 07:59:38 +0900

    fix: hostname

---
+++ tmux/scripts/ssh-status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -n "$SSH_CONNECTION" ]; then
+    SERVER_IP=$(echo "$SSH_CONNECTION" | awk '{print $3}')
+    echo "$USER@$SERVER_IP"
+elif [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+    echo "$USER@ssh"
+else
+    echo "$USER@local"
+fi
