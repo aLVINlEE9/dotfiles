2775192141b0e31a2eae0d13ea4dc76f940a10fc
aLVINlEE9 <fldh3746@gmail.com>
2025-06-29 19:47:14 +0900

    feat: ssh cp

--- nvim/lua/config/ssh.lua
+++ nvim/lua/config/ssh.lua
@@ -1,15 +1,16 @@
 if vim.env.SSH_CONNECTION then
 	local mac_username = "alvinlee"
+	local key_path = vim.fn.expand("~/.ssh/clipboard_key")
 
 	vim.g.clipboard = {
-		name = "mac-clipboard-ssh-tunnel",
+		name = "mac-clipboard-secure",
 		copy = {
-			["+"] = { "ssh", "-p", "22222", mac_username .. "@localhost", "pbcopy" },
-			["*"] = { "ssh", "-p", "22222", mac_username .. "@localhost", "pbcopy" },
+			["+"] = { "ssh", "-p", "22222", "-i", key_path, mac_username .. "@localhost", "copy" },
+			["-"] = { "ssh", "-p", "22222", "-i", key_path, mac_username .. "@localhost", "copy" },
 		},
 		paste = {
-			["+"] = { "ssh", "-p", "22222", mac_username .. "@localhost", "pbpaste" },
-			["*"] = { "ssh", "-p", "22222", mac_username .. "@localhost", "pbpaste" },
+			["+"] = { "ssh", "-p", "22222", "-i", key_path, mac_username .. "@localhost", "paste" },
+			["-"] = { "ssh", "-p", "22222", "-i", key_path, mac_username .. "@localhost", "paste" },
 		},
 		cache_enabled = false,
 	}
