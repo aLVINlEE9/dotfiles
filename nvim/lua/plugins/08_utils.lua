724724bdace40673b60e5d07b61920b371b07174
aLVINlEE9 <fldh3746@gmail.com>
2025-07-01 11:12:00 +0900

    feat: add config.local

--- nvim/lua/plugins/08_utils.lua
+++ nvim/lua/plugins/08_utils.lua
@@ -62,4 +62,17 @@ return {
 		"wakatime/vim-wakatime",
 		lazy = false,
 	},
+	{
+		"klen/nvim-config-local",
+		config = function()
+			require("config-local").setup({
+				config_files = { ".nvim.lua", ".nvimrc", ".exrc" },
+				hashfile = vim.fn.stdpath("data") .. "/config-local",
+				autocommands_create = true,
+				commands_create = true,
+				silent = false,
+				lookup_parents = false,
+			})
+		end,
+	},
 }
