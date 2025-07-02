338df684dff8bceb8e78c461d1b34c7ba23f4d98
aLVINlEE9 <fldh3746@gmail.com>
2025-07-02 23:12:43 +0900

    fix: whichkey

--- nvim/lua/plugins/08_utils.lua
+++ nvim/lua/plugins/08_utils.lua
@@ -22,25 +22,6 @@ return {
 		-- this is equivalent to setup({}) function
 	},
 	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-			notify = false,
-		},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-		},
-	},
-	{
 		"christoomey/vim-tmux-navigator",
 		cmd = {
 			"TmuxNavigateLeft",
