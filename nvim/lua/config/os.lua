ae2dab89b0f4e22854cc6ca444dcd44ab98a56c6
aLVINlEE9 <fldh3746@gmail.com>
2025-07-02 21:28:25 +0900

    feat: ssh tmux

--- nvim/lua/config/os.lua
+++ nvim/lua/config/os.lua
@@ -15,32 +15,4 @@ if vim.fn.has("win32") == 1 then
 			io.flush()
 		end,
 	})
-	if vim.env.SSH_CONNECTION then
-		local function nav(dir)
-			local curr = vim.fn.winnr()
-			vim.cmd("wincmd " .. dir)
-			if vim.fn.winnr() == curr then
-				local tmux_dir = ({ h = "L", j = "D", k = "U", l = "R" })[dir]
-				vim.fn.system("tmux select-pane -" .. tmux_dir)
-			end
-		end
-
-		vim.keymap.set("n", "<C-h>", function()
-			nav("h")
-		end, { silent = true })
-		vim.keymap.set("n", "<C-j>", function()
-			nav("j")
-		end, { silent = true })
-		vim.keymap.set("n", "<C-k>", function()
-			nav("k")
-		end, { silent = true })
-		vim.keymap.set("n", "<C-l>", function()
-			nav("l")
-		end, { silent = true })
-		vim.keymap.set("n", "<C-\\>", function()
-			vim.fn.system("tmux select-pane -l")
-		end, { silent = true })
-
-		vim.g.tmux_navigator_no_mappings = 1
-	end
 end
