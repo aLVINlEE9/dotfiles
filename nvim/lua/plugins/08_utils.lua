a9af11d6118361cdbea455f32dfdb2bd78ae5afe
aLVINlEE9 <fldh3746@gmail.com>
2025-07-02 18:38:28 +0900

    feat: ssh tmux

--- nvim/lua/plugins/08_utils.lua
+++ nvim/lua/plugins/08_utils.lua
@@ -57,6 +57,34 @@ return {
 			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
 			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
 		},
+		config = function()
+			local is_tmux = vim.env.TMUX ~= nil
+
+			if is_tmux then
+				vim.api.nvim_create_autocmd("VimEnter", {
+					group = vim.api.nvim_create_augroup("TmuxVimNavigator", { clear = true }),
+					callback = function()
+						io.write("\027]0;VIM_ACTIVE\007")
+						io.flush()
+
+						local success = pcall(vim.fn.system, "tmux set-environment VIM_TERMINAL 1")
+						if not success then
+							vim.env.VIM_TERMINAL = "1"
+						end
+					end,
+				})
+				vim.api.nvim_create_autocmd("VimLeave", {
+					group = vim.api.nvim_create_augroup("TmuxVimNavigator", { clear = false }),
+					callback = function()
+						io.write("\027]0;\007")
+						io.flush()
+
+						pcall(vim.fn.system, "tmux set-environment -u VIM_TERMINAL")
+						vim.env.VIM_TERMINAL = nil
+					end,
+				})
+			end
+		end,
 	},
 	{
 		"wakatime/vim-wakatime",
