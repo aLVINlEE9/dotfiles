aad4505a4965f86e9914cdf5a3154f204e995494
aLVINlEE9 <fldh3746@gmail.com>
2025-07-02 22:30:49 +0900

    feat: ssh tmux

--- nvim/lua/config/os.lua
+++ nvim/lua/config/os.lua
@@ -3,16 +3,4 @@ if vim.fn.has("win32") == 1 then
 	vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "
 	vim.o.shellquote = ""
 	vim.o.shellxquote = ""
-	vim.api.nvim_create_autocmd("VimEnter", {
-		callback = function()
-			io.write("\027]0;VIM_ACTIVE\007")
-			io.flush()
-		end,
-	})
-	vim.api.nvim_create_autocmd("VimLeave", {
-		callback = function()
-			io.write("\027]0;\007")
-			io.flush()
-		end,
-	})
 end
