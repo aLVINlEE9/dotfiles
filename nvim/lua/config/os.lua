3e2d578dae55a6ef51330f10fbbc58d61f05efc2
aLVINlEE9 <fldh3746@gmail.com>
2025-07-02 19:07:17 +0900

    feat: ssh tmux

--- nvim/lua/config/os.lua
+++ nvim/lua/config/os.lua
@@ -4,25 +4,43 @@ if vim.fn.has("win32") == 1 then
 	vim.o.shellquote = ""
 	vim.o.shellxquote = ""
 	vim.api.nvim_create_autocmd("VimEnter", {
-		group = vim.api.nvim_create_augroup("TmuxVimNavigator", { clear = true }),
 		callback = function()
 			io.write("\027]0;VIM_ACTIVE\007")
 			io.flush()
-
-			local success = pcall(vim.fn.system, "tmux set-environment VIM_TERMINAL 1")
-			if not success then
-				vim.env.VIM_TERMINAL = "1"
-			end
 		end,
 	})
 	vim.api.nvim_create_autocmd("VimLeave", {
-		group = vim.api.nvim_create_augroup("TmuxVimNavigator", { clear = false }),
 		callback = function()
 			io.write("\027]0;\007")
 			io.flush()
-
-			pcall(vim.fn.system, "tmux set-environment -u VIM_TERMINAL")
-			vim.env.VIM_TERMINAL = nil
 		end,
 	})
+	if vim.env.SSH_CONNECTION then
+		local function nav(dir)
+			local curr = vim.fn.winnr()
+			vim.cmd("wincmd " .. dir)
+			if vim.fn.winnr() == curr then
+				local tmux_dir = ({ h = "L", j = "D", k = "U", l = "R" })[dir]
+				vim.fn.system("tmux select-pane -" .. tmux_dir)
+			end
+		end
+
+		vim.keymap.set("n", "<C-h>", function()
+			nav("h")
+		end, { silent = true })
+		vim.keymap.set("n", "<C-j>", function()
+			nav("j")
+		end, { silent = true })
+		vim.keymap.set("n", "<C-k>", function()
+			nav("k")
+		end, { silent = true })
+		vim.keymap.set("n", "<C-l>", function()
+			nav("l")
+		end, { silent = true })
+		vim.keymap.set("n", "<C-\\>", function()
+			vim.fn.system("tmux select-pane -l")
+		end, { silent = true })
+
+		vim.g.tmux_navigator_no_mappings = 1
+	end
 end
