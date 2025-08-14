7d38088c57d6bedecc29b5721a1dae75f339bd09
aLVINlEE9 <fldh3746@gmail.com>
2025-08-14 17:17:40 +0900

    add cpp snippet

--- nvim/lua/plugins/04_cmp.lua
+++ nvim/lua/plugins/04_cmp.lua
@@ -12,6 +12,14 @@ return {
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 
+		luasnip.config.set_config({
+			history = true,
+			updateevents = "TextChanged,TextChangedI",
+			enable_autosnippets = true,
+		})
+
+		require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/lua/snippets/" })
+
 		cmp.setup({
 			completion = {
 				autocomplete = false,
