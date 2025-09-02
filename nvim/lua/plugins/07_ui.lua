ef78ff3bee7b27ee744c8f186efd337a45081ef6
aLVINlEE9 <fldh3746@gmail.com>
2025-09-02 10:51:08 +0900

    fix: prevent nvim-tree from changing root dir

--- nvim/lua/plugins/07_ui.lua
+++ nvim/lua/plugins/07_ui.lua
@@ -102,11 +102,11 @@ return {
 		},
 		config = function()
 			require("nvim-tree").setup({
-				sync_root_with_cwd = true,
-				respect_buf_cwd = true,
+				sync_root_with_cwd = false,
+				respect_buf_cwd = false,
 				update_focused_file = {
 					enable = true,
-					update_root = true,
+					update_root = false,
 				},
 			})
 		end,
