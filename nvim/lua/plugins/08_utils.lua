47ef3a506fc54d8b7d9b619fc6df39302054841f
aLVINlEE9 <fldh3746@gmail.com>
2025-09-28 19:33:18 +0900

    remove build with kittyconf

--- nvim/lua/plugins/08_utils.lua
+++ nvim/lua/plugins/08_utils.lua
@@ -77,7 +77,6 @@ return {
 	},
 	{
 		"mrjones2014/smart-splits.nvim",
-		build = "./kitty/install-kittens.bash",
 		config = function()
 			require("smart-splits").setup({
 				-- Ignored buffer types (only while resizing)
